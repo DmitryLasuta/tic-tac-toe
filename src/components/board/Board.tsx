12ed944f07553ab0c30d785b8c9e1d88327af1c3
DmitryLasuta <lasutadima01@gmail.com>
2023-10-04 02:51:13 +0300

    refactor: added lifting state up

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -3,13 +3,13 @@ import styles from './Board.module.css'
 
 type FieldValue = 'X' | 'O' | null
 
-function Square() {
-  const [value, setValue] = useState<FieldValue>(null)
-
-  const handleClick = () => {
-    setValue('X')
-  }
-
+function Square({
+  value,
+  handleClick,
+}: {
+  value: FieldValue
+  handleClick: () => void
+}) {
   return (
     <button className={styles.square} onClick={handleClick} type="button">
       {value}
@@ -18,17 +18,29 @@ function Square() {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [fieldsetValues, setFieldsetValues] = useState<FieldValue[]>(
+    Array(9).fill(null)
+  )
+
+  const handleClick = (index: number) => {
+    const newFieldsetValues = [...fieldsetValues]
+    xIsNext ? (newFieldsetValues[index] = 'X') : (newFieldsetValues[index] = 'O')
+    setFieldsetValues(newFieldsetValues)
+    setXIsNext(!xIsNext)
+  }
+
   return (
     <div className={styles.board}>
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      {fieldsetValues.map((fieldValue, index) => (
+        <Square
+          key={index}
+          value={fieldValue}
+          handleClick={() => {
+            handleClick(index)
+          }}
+        />
+      ))}
     </div>
   )
 }

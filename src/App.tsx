63f12e138945d25be732891e50bc78517bbb3565
DmitryLasuta <lasutadima01@gmail.com>
2023-10-04 15:17:17 +0300

    feat: added steps history

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,50 @@ import Board from './components/board/Board'
 import './App.css'
 
 function App() {
-  const [fieldset, setFieldset] = useState<FieldValue[]>(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState<Array<FieldValue[]>>([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useState(0)
+
+  const xIsNext = currentMove % 2 === 0
+  const currentFieldset = history[currentMove]
 
   const handlePlay = (fieldsetValues: FieldValue[]) => {
-    setXIsNext(!xIsNext)
-    setFieldset(fieldsetValues)
+    setHistory([...history, fieldsetValues])
+    setCurrentMove(history.length)
   }
 
   const handleRestart = () => {
-    setFieldset(Array(9).fill(null))
-    setXIsNext(true)
+    setHistory([Array(9).fill(null)])
+    setCurrentMove(0)
   }
+
+  const jumpTo = (step: number) => {
+    setCurrentMove(step)
+  }
+
+  const move = history.map((_, move) => {
+    if (move > 0) {
+      const desc = `Go to move #${move}`
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)} type="button">
+            {desc}
+          </button>
+        </li>
+      )
+    }
+  })
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board onPlay={handlePlay} xIsNext={xIsNext} fieldset={fieldset} />
+        <Board onPlay={handlePlay} xIsNext={xIsNext} fieldset={currentFieldset} />
       </div>
       <div className="game-control">
         <button onClick={handleRestart} type="button">
           Restart Game
         </button>
       </div>
-      <ol className="game-info">{/* TODO */}</ol>
+      <ol className="game-info">{move}</ol>
     </div>
   )
 }

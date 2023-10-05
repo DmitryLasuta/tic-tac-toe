a0f7c4a250fbce390c836ca991a76b3595cd1ece
DmitryLasuta <lasutadima01@gmail.com>
2023-10-06 02:28:57 +0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
 
-  resolve: {
-    alias: {
-      services: '/src/services',
-      types: '/src/types',
+    resolve: {
+      alias: {
+        services: '/src/services',
+        types: '/src/types',
+      },
     },
-  },
 
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
